6909eca76cbaabb1511c86f5574525b0611d2178
piyush karmakar <79916156+piyushkarmakar@users.noreply.github.com>
2023-08-21 13:45:49 +0530

    Update UnitTest.java

--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -4,6 +4,7 @@ import org.aspectj.lang.annotation.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.Select;
